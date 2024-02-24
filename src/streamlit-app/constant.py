e4ae804dc614b1c0577137f2d2974a5423884741
SaDamicis <s.damicis@ammagamma.com>
2024-02-24 10:39:15 +0100

    start lesson 4

---
+++ src/streamlit-app/constant.py
@@ -0,0 +1,5 @@
+import os 
+
+DATE_COLUMN = 'date/time'
+PROJECT_FOLDER = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+NETFLIX_PATH = os.path.join(PROJECT_FOLDER, 'data', 'netflix1.csv')
